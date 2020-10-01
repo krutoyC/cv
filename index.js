c9eeef70fa5b9a4b1991cf60cbeb37a85844d23c
Carlos M. Rodriguez <carlosnotengo1@gmail.com>
2020-09-30 22:59:09 -0300

    se agrego un reset de los campos del formulario en la ventana modal

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $("#exampleModal").on("show.bs.modal", function (event) {
   // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
   // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
   let modal = $(this);
-  
+
   modal.find(".modal-body input").val(recipient);
 });
 
@@ -37,53 +37,60 @@ function validarCheckBox() {
   if (checkBox.checked) {
     checkBox.className = "form-control is-valid";
     return true;
-      } else {
+  } else {
     checkBox.className = "form-control is-invalid";
     return false;
   }
- 
 }
 
-checkBox.addEventListener("change",validarCheckBox)
+checkBox.addEventListener("change", validarCheckBox);
 
-function validarGeneral(event){
-event.preventDefault();
-console.log("validarGeneral");
+function validarGeneral(event) {
+  event.preventDefault();
+  console.log("validarGeneral");
 
-if(revisar(document.getElementById("nombre"))&& revisar(document.getElementById("apellido"))&& validarEmail(document.getElementById("email"))&& revisar(document.getElementById("mensaje"))&& validarCheckBox()){
-  enviarEmail();
-}else{
-  alert("Error en el formulario");
+  if (
+    revisar(document.getElementById("nombre")) &&
+    revisar(document.getElementById("apellido")) &&
+    validarEmail(document.getElementById("email")) &&
+    revisar(document.getElementById("mensaje")) &&
+    validarCheckBox()
+  ) {
+    enviarEmail();
+  } else {
+    alert("Error en el formulario");
+  }
 }
-}
 
-function enviarEmail(){
+function enviarEmail() {
   let templateParams = {
-    
-    name:`De: ${document.getElementById("nombre").value}-${document.getElementById("apellido").value}`,
-    notes: `Email: ${document.getElementById("email").value}--Mensaje: ${document.getElementById("mensaje").value}`
-};
- console.log("eviar email")
-emailjs.send('gmail', 'template_OPnz7IWr', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-       if( document.getElementById('msjEnviado').className="alert alert-primary my-4"){
-        document.getElementById('msjEnviado').innerText="Su consulta fue enviada correctamente";
-        document.getElementById("nombre").value==="";
-       document.getElementById("apellido").value==="";
-       document.getElementById("email").value==="";
-       document.getElementById("mensaje").value==="";
-
-       }
-    
-
-    }, function(error) {
-       console.log('FAILED...', error);
-       document.getElementById('msjEnviado').className="alert alert-danger my-4";
-       document.getElementById('msjEnviado').innerText="Ocurrio un problema ajeno a este formulario";
-    });
-
-    
-
+    name: `De: ${document.getElementById("nombre").value}-${
+      document.getElementById("apellido").value
+    }`,
+    notes: `Email: ${document.getElementById("email").value}--Mensaje: ${
+      document.getElementById("mensaje").value
+    }`,
+  };
+  console.log("eviar email");
+  emailjs.send("gmail", "template_OPnz7IWr", templateParams).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+      document.getElementById("msjEnviado").className =
+        "alert alert-primary my-4";
+      document.getElementById("msjEnviado").innerText =
+        "Su consulta fue enviada correctamente";
+      limpiarFormlario();
+    },
+    function (error) {
+      console.log("FAILED...", error);
+      document.getElementById("msjEnviado").className =
+        "alert alert-danger my-4";
+      document.getElementById("msjEnviado").innerText =
+        "Ocurrio un problema ajeno a este formulario";
+    }
+  );
 }
 
+function limpiarFormlario() {
+  document.getElementById("miform").reset();
+}
